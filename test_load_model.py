eff134618c617ebb7513d63f795b34fdcd8a5819
sambt22022517 <22022517@vnu.edu.vn>
2024-03-29 20:41:07 +0700

    model: Huấn luyện lại model
    Huấn luyện lại model để fix bug xảy ra trong quá trình làm UI

--- test_load_model.py
+++ test_load_model.py
@@ -2,5 +2,6 @@ import joblib
 
 model = joblib.load('E:/Subject/University/Data_Mining_And_Analysis/data_mining_project/model/sam_model.pkl')
 
-print(model.predictFromComment('i hade it', type='label'))
-print(model.predictFromComment('i hade it', type='proba'))
+print(model.predictFromComment('i don\'t like it', type='label'))
+print(model.predictFromComment('i don\'t like it', type='proba'))
+
